6d74d27e1988ae01ee829b8cea1054abbc37e86d
GuiPado <guipadocastro@gmail.com>
2022-08-24 19:22:37 -0300

    Adiconado pasta Login

---
+++ LOGIN/JS/script_jquery.js
@@ -0,0 +1,11 @@
+$(function () {
+    $('btn-login').click(function(){
+        $('h2').css("color", "red");
+        /*e.preventDefault();
+        alert("Logado!!");*/
+    });
+});
+
+/*$(window).click(function(){
+    $('.card.Billet')
+    alert("teste");*/
